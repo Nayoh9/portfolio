27cabdef660092664a875666747604747ae92a66
Yohann Andre <y.andre90000@gmail.com>
2024-01-09 20:33:47 +0100

    add first part of the css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "white": "#FFFFFF",
+      "black": "#000000"
+    },
+    backgroundImage: {
+      "hero-banner": "url('https://res.cloudinary.com/dwkwlok28/image/upload/v1704811696/portfolio/cloud-background_idlorr.jpg')"
+    },
+    fontFamily: {
+      "mont": "Montserrat"
+    },
     extend: {},
   },
   plugins: [],
