aa678e1b796f50f01556a94a64340f43e3f6ada4
Yohann Andre <y.andre90000@gmail.com>
2024-01-10 20:03:09 +0100

    add new animations on home

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,48 @@ import {
   faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Typewriter, Cursor } from "react-simple-typewriter";
 
 library.add(faGear, faLinkedinIn, faInstagram, faWhatsapp);
 
 function App() {
   const [isGearClicked, setIsGearClicked] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   const handleOpenGearMenu = () => {
-    !isGearClicked ? setIsGearClicked(true) : setIsGearClicked(false);
+    isGearClicked ? setIsGearClicked(false) : setIsGearClicked(true);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => {
+        if (prevCounter === 6) {
+          clearInterval(interval);
+        }
+        return prevCounter + 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className="relative h-screen w-screen bg-white">
-      <section className="flex  h-4/6 flex-col font-mont text-white">
-        <div className="absolute top-0 z-0 h-4/6 w-full bg-hero-banner bg-cover bg-top"></div>
-        <div className="z-00 absolute top-0  h-4/6 w-full bg-black opacity-65"></div>
-        <header className="relative flex h-20 w-full items-center justify-between px-32">
+      <section
+        onClick={() => {
+          if (isGearClicked) {
+            setIsGearClicked(false);
+          }
+        }}
+        className="flex h-4/6 flex-col font-mont text-white"
+      >
+        <img
+          className="animate-fadeAnim absolute top-0 z-0 h-4/6  w-full"
+          src="https://res.cloudinary.com/dwkwlok28/image/upload/v1704811696/portfolio/cloud-background_idlorr.jpg"
+        />
+        <div className=" animate-fadeAnim z-00 absolute top-0  h-4/6 w-full bg-black opacity-65"></div>
+        <header className="animate-fadeHeader  relative flex h-20 w-full items-center justify-between px-32">
           <p className="font-semibold">
             <span className="text-xl font-extrabold">Y</span>ohann
           </p>
@@ -47,58 +73,82 @@ function App() {
           </nav>
         </header>
         <div className="sticky flex h-full w-full flex-col items-center justify-center gap-3 text-center">
-          <p className="text-lg font-semibold opacity-70">Hello</p>
+          <p className="text-lg font-semibold opacity-70">
+            {counter >= 4 && <Typewriter words={["Hello"]} typeSpeed={100} />}
+            {counter === 4 && <Cursor />}
+          </p>
+
           <h1 className="text-2xl font-semibold">
-            <span className="font-extralight">I Am</span> FULL STACK JUNIOR
-            DEVELOPER
+            <span className="font-extralight">
+              {counter >= 5 && <Typewriter words={["I Am"]} typeSpeed={100} />}
+              {counter === 5 && <Cursor />}
+            </span>
+            {counter >= 6 && (
+              <Typewriter
+                words={[" ANDRE Yohann", " FULL STACK JUNIOR DEVELOPER"]}
+                typeSpeed={100}
+              />
+            )}
+            {counter >= 6 && <Cursor />}
           </h1>
-          <div className="flex items-center gap-x-3">
+          <div className="animate-fadeHeader flex items-center gap-x-3">
             <img
               src="https://res.cloudinary.com/dwkwlok28/image/upload/v1704821683/portfolio/line-white_td9yia.svg"
               className="w-10 opacity-60"
             />
-            <p className="relative  text-xs font-extralight">ANDRE YOHANN</p>
+            <p className="relative text-xs font-extralight">ANDRE YOHANN</p>
             <img
               src="https://res.cloudinary.com/dwkwlok28/image/upload/v1704821683/portfolio/line-white_td9yia.svg"
               className="w-10 opacity-60"
             />
           </div>
-          <button className="rounded-xl bg-white px-5 py-1 text-sm font-semibold text-black transition-all ease-linear hover:bg-black hover:text-white">
+          <button className="animate-fadeHeader rounded-xl bg-white px-5 py-1 text-sm font-semibold text-black transition-all duration-500 ease-linear hover:bg-black hover:text-white">
             HIRE ME
           </button>
         </div>
 
-        <div className="absolute top-40">
-          <div className="flex h-6 w-6 items-center justify-center rounded-sm bg-white">
+        <div className="animate-fadeHeader absolute top-40 ">
+          <div
+            className="flex h-6 w-6 items-center justify-center rounded-sm bg-white"
+            onClick={handleOpenGearMenu}
+          >
             <FontAwesomeIcon
               icon="fa-solid fa-gear"
               style={{ color: "#000000" }}
-              className="h-3 w-3"
-              onClick={handleOpenGearMenu}
+              className="z-50 h-3 w-3"
             />
           </div>
 
           <div
-            style={{ display: isGearClicked ? "flex" : "none" }}
-            className="flex flex-col items-center justify-center gap-y-2 "
+            className={
+              isGearClicked
+                ? "flex flex-col items-center justify-center gap-y-2 transition-all duration-500 ease-out"
+                : "invisible flex -translate-y-5 flex-col items-center justify-center gap-y-2 opacity-0 transition-all duration-500 ease-out"
+            }
           >
             <img
               src="https://res.cloudinary.com/dwkwlok28/image/upload/v1704821683/portfolio/line-white_td9yia.svg"
               className="mt-7 w-10 rotate-90 opacity-60"
             />
-            <a href="https://www.linkedin.com/in/yohann-andr%C3%A9/">
+            <a
+              href="https://www.linkedin.com/in/yohann-andr%C3%A9/"
+              target="blank"
+            >
               <FontAwesomeIcon
                 icon="fa-brands fa-linkedin-in"
                 style={{ color: "#ffffff" }}
               />
             </a>
-            <a href="https://www.instagram.com/naysnayoh/?hl=en-gb">
+            <a
+              href="https://www.instagram.com/naysnayoh/?hl=en-gb "
+              target="blank"
+            >
               <FontAwesomeIcon
                 icon="fa-brands fa-instagram"
                 style={{ color: "#ffffff" }}
               />
             </a>
-            <a href="https://wa.me/33659613598">
+            <a href="https://wa.me/33659613598" target="blank">
               <FontAwesomeIcon
                 icon="fa-brands fa-whatsapp"
                 style={{ color: "#ffffff" }}
