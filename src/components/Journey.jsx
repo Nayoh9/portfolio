03464cb3791a04c5990de013226903292bfe97d4
Yohann Andre <y.andre90000@gmail.com>
2024-01-15 16:40:10 +0100

    add new divs on journey

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -1,6 +1,41 @@
+import { motion } from "framer-motion";
+
 const Journey = () => {
   return (
-    <section className="h-timeLineHeight w-screen bg-black text-white"></section>
+    <section className=" relative flex   flex-col items-center ">
+      <motion.img
+        src="src/assets/traitest.png"
+        alt=""
+        className=" h-timeLineHeight absolute left-1/2 right-1/2 w-10 "
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 0.5 }}
+        viewport={{ once: true }}
+      />
+      <article className=" flex w-9/12 flex-col items-end  ">
+        <div className="mb-20 flex w-1/2 flex-col items-start">
+          <h2>Zboub</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+            nesciunt quaerat sunt rem dignissimos! Animi doloremque inventore
+            provident! Consequatur quam amet facere, modi nam dolorum aut
+            suscipit minima ducimus natus.
+          </p>
+        </div>
+      </article>
+
+      <article className=" flex w-9/12 flex-col  ">
+        <div className="mb-20 flex w-1/2 flex-col items-start">
+          <h2>Zboub</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
+            nesciunt quaerat sunt rem dignissimos! Animi doloremque inventore
+            provident! Consequatur quam amet facere, modi nam dolorum aut
+            suscipit minima ducimus natus.
+          </p>
+        </div>
+      </article>
+    </section>
   );
 };
 
