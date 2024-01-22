6ebc4e99cb81633833d7d5f7ffd2ba44723a5322
Yohann Andre <y.andre90000@gmail.com>
2024-01-22 17:10:32 +0100

    fix space error between icon and link in portfolio component

--- src/components/PortfolioProject.jsx
+++ src/components/PortfolioProject.jsx
@@ -38,7 +38,7 @@ const PortfolioProject = ({ setIsDelayed, isDelayed }) => {
           </p>
         </div>
 
-        <div className="gap-x-1g my-6 flex justify-center">
+        <div className="gap-x-1g my-6 flex justify-center gap-x-1">
           <a href="https://github.com/Nayoh9?tab=repositories">
             <img
               src="/assets/stack portfolio/github_irxfsl.png"
