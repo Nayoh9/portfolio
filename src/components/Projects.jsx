e6f48367d38950512293875ec7af3c7fe1e53421
Yohann Andre <y.andre90000@gmail.com>
2024-02-05 18:27:03 +0100

    add some style to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,17 +10,17 @@ import KiessProject from "./KiessProject";
 
 const Projects = ({ isDelayed, setIsDelayed }) => {
   return (
-    <section className="mb-28 w-full pt-12 font-mont" id="projects">
+    <section className="mb-28 pt-12 font-mont" id="projects">
       <motion.h1
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 2 }}
         viewport={{ once: true }}
-        className="mb-20 text-center text-2xl font-semibold dark:text-white"
+        className=" mb-20 text-center text-2xl font-semibold dark:text-white"
       >
         My Projects
       </motion.h1>
-      <motion.div className="flex flex-wrap justify-center gap-10 ">
+      <motion.div className="flex flex-wrap justify-center gap-10">
         <MarvelProject isDelayed={isDelayed} />
         <ScansipProject isDelayed={isDelayed} />
         <PortfolioProject isDelayed={isDelayed} />
